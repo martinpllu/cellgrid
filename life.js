cd7db0edbfb09b47d4490043df4366cad8e54b3b
Martin Pllu <martin_pllu@standardlife.com>
2014-01-04 16:22:27 +0000

    After life perf changes - using for loops instead of callbacks speeds up by 3x and avoids JIT delay

--- life.js
+++ life.js
@@ -17,34 +17,44 @@ Life.prototype.init = function () {
 };
 
 Life.prototype.tick = function () {
-    var life = this;
-    this.grid.eachCell(function (cell, x, y) {
-        var n = life.liveNeighbours(cell);
-        if (cell.state === 1) {
-            if (n < 2) {
-                cell.nextState = 0;
-            } else if (n > 3) {
-                cell.nextState = 0;
-            } else {
-                cell.nextState = 1;
-            }
-        } else if (cell.state === 0) {
-            if (n === 3) {
-                cell.nextState = 1;
-            } else {
-                cell.nextState = 0;
+    var x,
+        y,
+        cell,
+        n;
+    for (x = 0; x < this.grid.width; x++) {
+        for (y = 0; y < this.grid.height; y++) {
+            cell = this.grid.cells[x][y];
+            n = this.liveNeighbours(cell);
+            if (cell.state === 1) {
+                if (n < 2) {
+                    cell.nextState = 0;
+                } else if (n > 3) {
+                    cell.nextState = 0;
+                } else {
+                    cell.nextState = 1;
+                }
+            } else if (cell.state === 0) {
+                if (n === 3) {
+                    cell.nextState = 1;
+                } else {
+                    cell.nextState = 0;
+                }
             }
         }
-    });
-    this.grid.eachCell(function (cell) {
-        cell.state = cell.nextState;
-    });
+    }
+    for (x = 0; x < this.grid.width; x++) {
+        for (y = 0; y < this.grid.height; y++) {
+            cell = this.grid.cells[x][y];
+            cell.state = cell.nextState;
+        }
+    }
 };
 
 Life.prototype.liveNeighbours = function (cell) {
-    var result = 0;
-    cell.eachNeighbour(function (neighbour) {
-        result += neighbour.state;
-    });
+    var result = 0,
+        i;
+    for (i = 0; i < cell.neighbours.length; i++) {
+        result += cell.neighbours[i].state;
+    }
     return result;
 };
